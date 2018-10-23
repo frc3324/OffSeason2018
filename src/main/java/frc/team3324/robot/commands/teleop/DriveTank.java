8dcf8f2db37e36f521c90b8d5ff73806a6ec305a
maniyar1 <kamalisarvestani.1@themetroschool.org>
2018-10-22 20:48:05 -0400

    Format Test

--- src/main/java/frc/team3324/robot/commands/teleop/DriveTank.java
+++ src/main/java/frc/team3324/robot/commands/teleop/DriveTank.java
@@ -1,21 +1,14 @@
 package frc.team3324.robot.commands.teleop;
 
-import frc.team3324.robot.OI;
-import frc.team3324.robot.Robot;
-
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.GenericHID;
+import frc.team3324.robot.OI;import frc.team3324.robot.Robot;import edu.wpi.first.wpilibj.command.Command;import edu.wpi.first.wpilibj.GenericHID;
 
 public class DriveTank extends Command {
 
     public DriveTank() { requires(Robot.mDriveTrain); }
 
-    protected void execute() {
-        double leftY = OI.gamepad0.getY(GenericHID.Hand.kLeft); // Get the Y (Up/Down) value of the LEFT Joystick
+    protected void execute() { double leftY = OI.gamepad0.getY(GenericHID.Hand.kLeft); // Get the Y (Up/Down) value of the LEFT Joystick
         double leftX = OI.gamepad0.getX(GenericHID.Hand.kLeft); // Get the X (Left/Right) value of the LEFT Joystick
-
-        if (OI.gamepad0.getBumper(GenericHID.Hand.kRight)) { leftY *= 0.5; }
-        Robot.mDriveTrain.mDrive.arcadeDrive(leftY, -leftX, true);
+        if (OI.gamepad0.getBumper(GenericHID.Hand.kRight)) { leftY *= 0.5; }Robot.mDriveTrain.mDrive.arcadeDrive(leftY, -leftX, true);
     }
 
     @Override
